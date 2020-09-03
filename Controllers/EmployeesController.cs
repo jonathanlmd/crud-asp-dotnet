641b1969f6c296c1fd30e0f0b1f14f15885c84c4
Jonathan Lucas de Matos Diniz <jonathan.lucas.m@gmail.com>
2020-09-03 15:12:15 -0300

    Add employees list

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,16 +1,29 @@
 using System;
+using crud.Database;
 using crud.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace crud.Controllers {
+
 	public class EmployeesController : Controller {
+		private readonly ApplicationDBContext database;
+		public EmployeesController(ApplicationDBContext database) {
+			this.database = database;
+		}
+		public IActionResult Index() {
+			var employees = database.Employees.ToList();
+			return View(employees);
+		}
 		public IActionResult Register() {
 			return View();
 		}
 
 		[HttpPost]
 		public IActionResult Save(Employee employee) {
-			return Content("Saved");
+			database.Employees.Add(employee);
+			database.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
